ef5f332893b24560b2b41ddd029d0677cf507b8f
Lafor8 <jmtw_84@yahoo.com>
2014-10-09 15:58:20 +0800

    Revised the camera matrix used. Moved StereoRectify to run on before TriangulatePoints instead of only in initialization. Implemented the extraction of the Rotation and Translation Matrices (based on the Essential Matrix, taken from the Fundamental Matrix)

--- VINS/src/dlsu/vins/FastActivity.java
+++ VINS/src/dlsu/vins/FastActivity.java
@@ -1,7 +1,6 @@
 package dlsu.vins;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -17,23 +16,16 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.core.TermCriteria;
-import org.opencv.features2d.DescriptorExtractor;
-import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.Features2d;
 import org.opencv.features2d.KeyPoint;
 import org.opencv.video.Video;
 
 import android.app.Activity;
-import android.graphics.Canvas;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -131,50 +123,10 @@ public class FastActivity extends Activity implements CvCameraViewListener2 {
     private Mat R1,R2,P1,P2,Q;
     private Mat points4D;
 
-    public void onCameraViewStarted(int width, int height) {
-    	 
+    public void onCameraViewStarted(int width, int height) {  	 
         drawMasks = new ArrayList<Mat>();
-    	
-    	// INITIALIZATION FOR STEREORECTIFY
-        
-        // INPUT VARIABLES
-        
-        cameraMatrix = Mat.zeros(3, 3, CvType.CV_64F);
-        distCoeffs =  Mat.zeros(5, 1, CvType.CV_64F);
-        imageSize = new Size(1920, 1080);
-        Rot = Mat.zeros(3, 3, CvType.CV_64F);
-        T = Mat.ones(3, 1, CvType.CV_64F);
-        
-        cameraMatrix.put(0, 0, 1768.104971372035, 0, 959.5);
-        cameraMatrix.put(1, 0, 0, 1768.104971372035, 539.5);
-        cameraMatrix.put(2, 0, 0, 0, 1);
-        
-        distCoeffs.put(0, 0, 0.1880897270445046);
-        distCoeffs.put(1, 0, -0.7348187497379466);
-        distCoeffs.put(2, 0, 0);
-        distCoeffs.put(3, 0, 0);
-        distCoeffs.put(4, 0, 0.6936210153459164);
-        
-        Rot.put(0, 0, 1, 0, 0);
-        Rot.put(1, 0, 0, 1, 0);
-        Rot.put(2, 0, 0, 0, 1);
-        
-        // OUTPUT VARIABLES
-        
-        R1 = Mat.zeros(3, 3, CvType.CV_64F);
-        R2 = Mat.zeros(3, 3, CvType.CV_64F);
-        P1 = Mat.zeros(3, 4, CvType.CV_64F);
-        P2 = Mat.zeros(3, 4, CvType.CV_64F);
-        Q = Mat.zeros(4, 4, CvType.CV_64F);
-        
-        // INITIALIZATION END
-        
-        // CALL STEREORECTIFY EACH FRAME AFTER THE FIRST
-        // JUST PASS A NEW ROTATION AND TRANSLATION MATRIX
-        
-        Calib3d.stereoRectify(cameraMatrix, distCoeffs, cameraMatrix.clone(), distCoeffs.clone(), imageSize, Rot, T, R1, R2, P1, P2, Q);
-        
-        
+        nullMatF = Mat.zeros(0, 0, CvType.CV_64F);
+        initRectifyVariables();
     }
 
     public void onCameraViewStopped() { }
@@ -195,6 +147,10 @@ public class FastActivity extends Activity implements CvCameraViewListener2 {
     // NOTE: just temporary variables
     private int triPointsKeep = 5;
     private int framesFade = 3;
+    
+    private Mat F, E, W;
+    private Mat u, w, vt;
+    private Mat nullMatF, temp;
     
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         Log.d("VINS", "onCameraFrame");
@@ -246,19 +202,70 @@ public class FastActivity extends Activity implements CvCameraViewListener2 {
             //		 so that both memory and time will be saved (instead of reallocation each time)
             //		 consider converting to Euclidean, but maybe no need.
             
-            points4D = Mat.zeros(1, 4, CvType.CV_64F);
-            
-            if(!goodOld.empty() && !goodOld.empty())
-            	Calib3d.triangulatePoints(P1, P2, goodOld, goodNew, points4D);
-            
-            // Only dumps a few sets of triangulation results for now
-            if(triPointsKeep > 0){
-                Log.i("Array Sizes", goodOld.size() + " " + goodNew.size() + " " + points4D.size());
-                Log.i("Old Points", goodOld.dump());
-                Log.i("New Points", goodNew.dump());
-            	Log.i("Triangulated Points", points4D.dump());
-            	triPointsKeep--;
-            }
+			points4D = Mat.zeros(1, 4, CvType.CV_64F);
+
+			if (!goodOld.empty() && !goodOld.empty()) {
+				// SOLVING FOR THE ROTATION AND TRANSLATION MATRICES
+				
+				// GETTING THE FUNDAMENTAL MATRIX
+				
+				F = Calib3d.findFundamentalMat(goodOld, goodNew);
+
+				cameraMatrix = cameraMatrix.clone();
+
+				temp = nullMatF.clone();
+				E = nullMatF.clone();
+
+				// GETTING THE ESSENTIAL MATRIX
+				
+				Core.gemm(cameraMatrix.t(), F, 1, nullMatF, 0, temp);
+				Core.gemm(temp, cameraMatrix, 1, nullMatF, 0, E);
+				
+				W = Mat.zeros(3, 3, CvType.CV_64F);
+            	W.put(0, 1, -1);
+            	W.put(1, 0, 1);
+            	W.put(2, 2, 1);
+            	u = nullMatF.clone();
+            	w = nullMatF.clone();
+            	vt = nullMatF.clone();
+            	
+				// DECOMPOSING ESSENTIAL MATRIX TO GET THE ROTATION AND TRANSLATION MATRICES
+            	
+            	// Decomposing Essential Matrix to obtain Rotation and Translation Matrices
+            	Core.SVDecomp(E, w, u, vt);
+
+            	Core.gemm(u, W, 1, nullMatF, 0, temp);
+            	Core.gemm(temp, vt, 1, nullMatF, 0, Rot);
+            	T = u.col(2);
+
+				// (DEBUG) LOGGING THE VARIOUS MATRICES
+            	
+//            	Log.i("E", E.dump());
+//            	Log.i("K", cameraMatrix.dump());
+//            	Log.i("F", F.dump());
+//            	Log.i("K", cameraMatrix.dump());
+//            	Log.i("E", E.dump());
+//            	Log.i("w", w.dump());
+//            	Log.i("u", u.dump());
+//            	Log.i("vt", vt.dump());
+//            	Log.i("r", Rot.dump());
+//            	Log.i("t", T.dump());
+//            	Log.i("nullMatF", nullMatF.dump());
+            	
+            	// RECTIFICATION AND TRIANGULATION
+            	
+            	Calib3d.stereoRectify(cameraMatrix, distCoeffs, cameraMatrix.clone(), distCoeffs.clone(), imageSize, Rot, T, R1, R2, P1, P2, Q);
+                Calib3d.triangulatePoints(P1, P2, goodOld, goodNew, points4D);
+                
+                // Only dumps a few sets of triangulation results for now
+                if(true){//triPointsKeep > 0){
+                    Log.i("Array Sizes", goodOld.size() + " " + goodNew.size() + " " + points4D.size());
+                    Log.i("Old Points", goodOld.dump());
+                    Log.i("New Points", goodNew.dump());
+                	Log.i("Triangulated Points", points4D.dump());
+                	triPointsKeep--;
+                }
+			}
         }
         
         if (frames % detectInterval == 0) {
@@ -313,4 +320,56 @@ public class FastActivity extends Activity implements CvCameraViewListener2 {
     public boolean onOptionsItemSelected(MenuItem item) {
         return true;
     }
+    
+    private void initRectifyVariables(){
+    	// INITIALIZATION FOR STEREORECTIFY()
+        
+        // INPUT VARIABLES
+        
+        cameraMatrix = Mat.zeros(3, 3, CvType.CV_64F);
+        distCoeffs =  Mat.zeros(5, 1, CvType.CV_64F);
+        imageSize = new Size(1920, 1080);
+        Rot = Mat.zeros(3, 3, CvType.CV_64F);
+        T = Mat.ones(3, 1, CvType.CV_64F);
+        
+        // CALIBRATION RESULTS FOR 320 x 240
+        cameraMatrix.put(0, 0, 287.484405747163, 0, 159.5);
+        cameraMatrix.put(1, 0, 0, 287.484405747163, 119.5);
+        cameraMatrix.put(2, 0, 0, 0, 1);
+        
+        distCoeffs.put(0, 0, 0.1831508618865668);
+        distCoeffs.put(1, 0, -0.8391135375141514);
+        distCoeffs.put(2, 0, 0);
+        distCoeffs.put(3, 0, 0);
+        distCoeffs.put(4, 0, 1.067914298622483);
+        
+        Rot.put(0, 0, 1, 0, 0);
+        Rot.put(1, 0, 0, 1, 0);
+        Rot.put(2, 0, 0, 0, 1);
+        
+        // OUTPUT VARIABLES
+        
+        R1 = Mat.zeros(3, 3, CvType.CV_64F);
+        R2 = Mat.zeros(3, 3, CvType.CV_64F);
+        P1 = Mat.zeros(3, 4, CvType.CV_64F);
+        P2 = Mat.zeros(3, 4, CvType.CV_64F);
+        Q = Mat.zeros(4, 4, CvType.CV_64F);
+        
+        // INITIALIZATION END
+        
+        // CALL STEREORECTIFY EACH FRAME AFTER THE FIRST
+        // JUST PASS A NEW ROTATION AND TRANSLATION MATRIX
+        
+//        CALIBRATION RESULTS FOR 1920 x 1080
+//        cameraMatrix.put(0, 0, 1768.104971372035, 0, 959.5);
+//        cameraMatrix.put(1, 0, 0, 1768.104971372035, 539.5);
+//        cameraMatrix.put(2, 0, 0, 0, 1);
+//        
+//        distCoeffs.put(0, 0, 0.1880897270445046);
+//        distCoeffs.put(1, 0, -0.7348187497379466);
+//        distCoeffs.put(2, 0, 0);
+//        distCoeffs.put(3, 0, 0);
+//        distCoeffs.put(4, 0, 0.6936210153459164);
+    }
 }
+
